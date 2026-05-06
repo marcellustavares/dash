517edf671464c4e9879b1cffef02ac40550493b9
Marcellus Tavares <marcellustavares@gmail.com>
2026-05-05 21:53:18 -0700

    naive version

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"maps"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Measurements struct {
+	min   float64
+	max   float64
+	sum   float64
+	count int
+}
 
 func main() {
-	fmt.Print("Hello World")
+	file, err := os.Open("data/sample_data.txt")
+	if err != nil {
+		log.Panic("Unable to open file")
+	}
+	defer file.Close()
+
+	m := make(map[string]Measurements)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		pair := strings.Split(line, ";")
+		station := pair[0]
+		temp, _ := strconv.ParseFloat(pair[1], 64)
+		measurements, found := m[station]
+		if !found {
+			measurements = Measurements{temp, temp, temp, 1}
+		} else {
+			measurements.min = min(temp, measurements.min)
+			measurements.max = max(temp, measurements.max)
+			measurements.sum += temp
+			measurements.count += 1
+		}
+		m[station] = measurements
+	}
+	fmt.Printf("{")
+	for i, station := range slices.Sorted(maps.Keys(m)) {
+		measurements := m[station]
+
+		if i > 0 {
+			fmt.Printf(", ")
+		}
+
+		fmt.Printf(
+			"%s=%.1f/%.1f/%.1f",
+			station,
+			measurements.min,
+			measurements.sum/float64(measurements.count),
+			measurements.max)
+
+	}
+	fmt.Printf("}")
 }
